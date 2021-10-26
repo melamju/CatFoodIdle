f0a10b0a7d02207049d51a9c219401f919a00306
melamju <bobrowski@mail.hs-ulm.de>
2021-10-26 16:59:17 +0200

    initial commit, basic openGL project

---
+++ game.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Eli on 21.10.2021.
+//
+
+#include "game.h"
+
+Game::Game(unsigned int width, unsigned int height)
+        : State(GAME_ACTIVE), Keys(), Width(width), Height(height)
+{
+
+}
+
+Game::~Game()
+{
+
+}
+
+void Game::Init()
+{
+
+}
+
+void Game::Update(float dt)
+{
+
+}
+
+void Game::ProcessInput(float dt)
+{
+
+}
+
+void Game::Render()
+{
+
+}
---
+++ stb_image.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Eli on 21.10.2021.
+//
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
